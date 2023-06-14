e9e9c055862901a773a10195f3ef35afa3754e90
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-06-14 11:26:53 +0100

    Abandon Paper & Java for Fabric & Kotlin

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# BedTeleport
+# Bed Teleport
 
-Lets players teleport back to their bed respawn point.
+This is a Minecraft mod for [Fabric](https://fabricmc.net/) that adds a `/bed` chat command to teleport players back to their respawn point bed, costing them experience based on distance.
+
+Development of the [Paper](https://papermc.io/) plugin concluded in May of 2021. It is still available on the [paper branch](https://github.com/viral32111/bed-teleport/tree/paper) and [1.0.0 release](https://github.com/viral32111/bed-teleport/releases/tag/1.0.0).
 
 ## License
 
-Copyright (C) 2021-2022 [viral32111](https://viral32111.com).
+Copyright (C) 2021-2023 [viral32111](https://viral32111.com).
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
---
+++ src/main/kotlin/com/viral32111/bedteleport/BedTeleport.kt
@@ -0,0 +1,73 @@
+package com.viral32111.bedteleport
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import net.fabricmc.api.DedicatedServerModInitializer
+import net.fabricmc.loader.api.FabricLoader
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@Suppress( "UNUSED" )
+class BedTeleport: DedicatedServerModInitializer {
+
+	companion object {
+		private const val MOD_ID = "bedteleport"
+		val LOGGER: Logger = LoggerFactory.getLogger( "com.viral32111.$MOD_ID" )
+
+		@OptIn( ExperimentalSerializationApi::class )
+		val JSON = Json {
+			prettyPrint = true
+			prettyPrintIndent = "\t"
+			ignoreUnknownKeys = true
+		}
+
+		const val CONFIGURATION_DIRECTORY_NAME = "viral32111"
+		const val CONFIGURATION_FILE_NAME = "$MOD_ID.json"
+
+		//var configuration = Configuration()
+
+		/**
+		 * Gets the current version of this mod.
+		 * @since 2.0.0
+		 */
+		fun getModVersion(): String =
+			FabricLoader.getInstance().getModContainer( MOD_ID ).orElseThrow {
+				throw IllegalStateException( "Mod container not found" )
+			}.metadata.version.friendlyString
+	}
+
+	override fun onInitializeServer() {
+		LOGGER.info( "Bed Teleport v${ getModVersion() } initialized on the server." )
+
+		//configuration = loadConfigurationFile()
+	}
+
+	/*private fun loadConfigurationFile(): Configuration {
+		val serverConfigurationDirectory = FabricLoader.getInstance().configDir
+		val configurationDirectory = serverConfigurationDirectory.resolve( CONFIGURATION_DIRECTORY_NAME )
+		val configurationFile = configurationDirectory.resolve( CONFIGURATION_FILE_NAME )
+
+		if ( configurationDirectory.notExists() ) {
+			configurationDirectory.createDirectory()
+			LOGGER.info( "Created directory '${ configurationDirectory }' for configuration files." )
+		}
+
+		if ( configurationFile.notExists() ) {
+			val configAsJSON = JSON.encodeToString( Configuration() )
+
+			configurationFile.writeText( configAsJSON, options = arrayOf(
+				StandardOpenOption.CREATE_NEW,
+				StandardOpenOption.WRITE
+			) )
+
+			LOGGER.info( "Created configuration file '${ configurationFile }'." )
+		}
+
+		val configAsJSON = configurationFile.readText()
+		val config = JSON.decodeFromString<Configuration>( configAsJSON )
+		LOGGER.info( "Loaded configuration from file '${ configurationFile }'" )
+
+		return config
+	}*/
+
+}
